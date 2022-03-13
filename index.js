2bef88a1f04d1ccb462bc122b06c7c4bcb4eafad
VivekCh-003 <99497682+VivekCh-003@users.noreply.github.com>
2022-03-13 22:03:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ setInterval(() => {
     const adv_mins = minutes < 10 ? '0' + minutes : minutes;
     const ampm = hour >= 12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML = (hoursIn12HrFormat < 12 ? ('0'+hoursIn12HrFormat) : hoursIn12HrFormat) + ':' + adv_mins + " " + `<span id="am-pm">${ampm}</span>`;
+    timeEl.innerHTML = (hoursIn12HrFormat < 10 ? ('0'+hoursIn12HrFormat) : hoursIn12HrFormat) + ':' + adv_mins + " " + `<span id="am-pm">${ampm}</span>`;
     dateEl.innerHTML = days[day] + ',' + date + ' ' + months[month] + ' ' + year;
 
 }, 1000);
